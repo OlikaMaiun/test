4175f50fbe752b2a6c0d9a721e259cca9b4e57b6
Olika <olia.maiun@gmail.com>
2017-10-27 17:50:01 +0300

    delete and change peace of code

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-function slag ($name, $surname){
+function slag ($name){
     if($name=="Olha"){
         print "You can enter.";
     } else {
@@ -7,31 +7,5 @@ function slag ($name, $surname){
     }
 }
 
-slag("Olha", "Maiun");
+slag("Olha");
 
-$number = "7";
-$arr = array();
-$numb = intval($number);
-if($numb==1){
-    echo 1;
-}else{
-    while ($numb!=1 || ! in_array($numb,$arr)){
-        $arr[]=$numb;
-        if ($numb < 10){
-            $numb = $numb * $numb;
-        }elseif ($numb >= 10){
-            $arr1= array();
-            for ($i=0; $i<=strlen($number); $i++){
-                $arr1[] = $i*$i;
-            }
-            $numb = array_sum($arr1);
-        }
-        if ($numb==1){
-            echo 1;
-
-        }elseif(in_array($numb,$arr)){
-            echo 0;
-
-        }
-    }
-}
