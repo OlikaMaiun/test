1f0f3c59d1d863576f94ac3538e28e7e90abb8bb
Olika <olia.maiun@gmail.com>
2017-11-02 13:46:19 +0200

    add new task

--- capitalizeWords.php
+++ capitalizeWords.php
@@ -1,4 +1,8 @@
 <?php
 function capitalizeWords($string){
-
+$string = ucwords($string);
+return$string. "\n";
 }
+echo capitalizeWords("Hello world");
+echo capitalizeWords("javaScript language");
+echo capitalizeWords("a letter");
